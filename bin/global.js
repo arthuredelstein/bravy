fd4386d5679b852a9cd5e1af73064272834b9cd8
Arthur Edelstein <arthuredelstein@gmail.com>
2022-09-08 15:42:39 -0700

    both and debug flags

--- bin/global.js
+++ bin/global.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
+const minimist = require('minimist');
+
 var override_library = require('../lib/override.js');
 
 // Displays the text in the console
-override_library.override(process.argv[2]);
+const { both, _ : argumentList } = minimist(process.argv.slice(2));
+override_library.override(argumentList[0], { both });
