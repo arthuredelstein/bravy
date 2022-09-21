af468c965ba3b8a12e8cf25acb0a762a38bc86f0
Arthur Edelstein <arthuredelstein@gmail.com>
2022-09-20 19:32:50 -0700

    beginnings of feature flag generation

--- bin/global.js
+++ bin/global.js
@@ -3,7 +3,12 @@
 const minimist = require('minimist');
 
 const overrideLibrary = require('../lib/override.js');
+const flagLibrary = require('../lib/flag.js');
 
 // Displays the text in the console
 const { both, _: argumentList } = minimist(process.argv.slice(2));
-overrideLibrary.override(argumentList[0], { both });
+if (argumentList[0] === "override") {
+  overrideLibrary.override(argumentList[1], { both });
+} else if (argumentList[0] === "flag") {
+  flagLibrary.flag(argumentList[1]);
+}
